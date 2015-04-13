d718506ef0723d634fdd33068b9840ae9394c021
Stephen Perrins <perrins.stephen@googlemail.com>
2015-04-13 17:47:43 +0100

    Action bar has app logo and mineral name in it.

--- app/src/main/java/com/example/jamesbell/killhope/NavigationDrawerFragment.java
+++ app/src/main/java/com/example/jamesbell/killhope/NavigationDrawerFragment.java
@@ -257,9 +257,9 @@ public class NavigationDrawerFragment extends Fragment {
      */
     private void showGlobalContextActionBar() {
         ActionBar actionBar = getActionBar();
-        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setTitle(R.string.app_name);
+
     }
 
     private ActionBar getActionBar() {
