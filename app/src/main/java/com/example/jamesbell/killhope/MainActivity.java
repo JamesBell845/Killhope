f21d19ec88dfccddda25d7ddac0ccb3e888029d9
Stephen Perrins <perrins.stephen@googlemail.com>
2015-04-15 15:02:56 +0100

    changed fragment_main to fragment_mineral. Added fragment_glossary

--- app/src/main/java/com/example/jamesbell/killhope/MainActivity.java
+++ app/src/main/java/com/example/jamesbell/killhope/MainActivity.java
@@ -6,10 +6,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,11 +15,8 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
-import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -178,7 +172,7 @@ public class MainActivity extends ActionBarActivity
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_mineral, container, false);
 
             ExpandableListView expandable = (ExpandableListView) rootView.findViewById(R.id.lvExp);
 
