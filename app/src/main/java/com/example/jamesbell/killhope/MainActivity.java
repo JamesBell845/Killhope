477b6e68b17106816e031a9bc8f3e739fe65182e
James Bell <jebell845@googlemail.com>
2015-04-15 14:20:16 +0100

    It's so pretty now..

--- app/src/main/java/com/example/jamesbell/killhope/MainActivity.java
+++ app/src/main/java/com/example/jamesbell/killhope/MainActivity.java
@@ -216,33 +216,6 @@ public class MainActivity extends ActionBarActivity
             listDataHeader.add("Stage 3");
             listDataHeader.add("Stage 4");
 
-            List<String> top250 = new ArrayList<String>();
-            top250.add("The Shawshank Redemption");
-            top250.add("The Godfather");
-            top250.add("The Godfather: Part II");
-            top250.add("Pulp Fiction");
-            top250.add("The Good, the Bad and the Ugly");
-            top250.add("The Dark Knight");
-            top250.add("12 Angry Men");
-
-            List<String> nowShowing = new ArrayList<String>();
-            nowShowing.add("The Conjuring");
-            nowShowing.add("Despicable Me 2");
-            nowShowing.add("Turbo");
-            nowShowing.add("Grown Ups 2");
-            nowShowing.add("Red 2");
-            nowShowing.add("The Wolverine");
-
-            List<String> comingSoon = new ArrayList<String>();
-            comingSoon.add("2 Guns");
-            comingSoon.add("The Smurfs 2");
-            comingSoon.add("The Spectacular Now");
-            comingSoon.add("The Canyons");
-            comingSoon.add("Europa Report");
-
-            listDataChild.put(listDataHeader.get(0), top250);
-            listDataChild.put(listDataHeader.get(1), nowShowing);
-            listDataChild.put(listDataHeader.get(2), comingSoon);
         }
 
         @Override
