fb916c2f2238d5c9bf0b938cef271c9c5f83c1da
James Bell <jebell845@googlemail.com>
2015-04-17 11:41:43 +0100

    History

---
+++ app/src/main/java/com/example/jamesbell/killhope/AspectRatioImageView.java
@@ -0,0 +1,28 @@
+package com.example.jamesbell.killhope;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+/**
+ * Created by jamesbell on 17/04/15.
+ */
+public class AspectRatioImageView extends ImageView{
+    public AspectRatioImageView(Context context) {
+        super(context);
+    }
+
+    public AspectRatioImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AspectRatioImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = width * getDrawable().getIntrinsicHeight() / getDrawable().getIntrinsicWidth();
+        setMeasuredDimension(width, height);
+    }
+}
